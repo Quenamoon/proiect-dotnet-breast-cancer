db3adbf3a6b2833fb6503ce63688a361fc7d48e4
tucaEmanuel18 <emanuel.tuca@info.uaic.ro>
2021-12-09 08:41:47 +0200

    Create DoctorPage

--- frontend/src/Components/MainContent.jsx
+++ frontend/src/Components/MainContent.jsx
@@ -1,49 +1,51 @@
 import "./MainContent.css";
 import "./MainContentTable.css";
+import { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const MainContent = () => {
+  let [patientsList, setPatientsList] = useState([]);
+  let [patientsCounter, setPatientsCounter] = useState(0);
+  let currentDoctorToken = useSelector((state) => state.auth.JWToken);
+
+  let fetchPatients = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://localhost:5001/api/users/patients",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer " + currentDoctorToken,
+          }
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Patients Get Request Went Wrong");
+      }
+      const data = await response.json();
+
+      const transformedPatients = data.map((patient) =>{
+        console.log("patient = " + patient);
+        return {
+          email: patient.email,
+          id: patient.id
+        };
+      });
+      // [{email: "test", id: "5"}, {email: "test2", id: "2"}]
+      setPatientsList(transformedPatients);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  
+  useEffect(() => {
+    fetchPatients();
+  }, [fetchPatients]);
+
   return (
     <div className="content">
-      {/* <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script type="text/javascript">
-        $(function () {
-            $("#tr-btn1-1").hide();
-            $("#tr-btn1-2").hide();
-            $("#tr-btn1-3").hide();
-            $("#tr-btn2-1").hide();
-            $("#tr-btn2-2").hide();
-            $("#tr-btn2-3").hide();
-            $("#btn1").click(function () {
-                $('.buttonInactive').not(this).removeClass('buttonInactive');
-                $(this).toggleClass('buttonActive');
-                if ($(this).hasClass("buttonActive")) {
-                    $("#tr-btn1-1").show();
-                    $("#tr-btn1-2").show();
-                    $("#tr-btn1-3").show();
-                }
-                else {
-                  $("#tr-btn1-1").hide();
-                  $("#tr-btn1-2").hide();
-                  $("#tr-btn1-3").hide();
-                }
-            });
-            $("#btn2").click(function () {
-                $('.buttonInactive').not(this).removeClass('buttonInactive');
-                $(this).toggleClass('buttonActive');
-                if ($(this).hasClass("buttonActive")) {
-                    $("#tr-btn2-1").show();
-                    $("#tr-btn2-2").show();
-                    $("#tr-btn2-3").show();
-                }
-                else {
-                    $("#tr-btn2-1").hide();
-                    $("#tr-btn2-2").hide();
-                    $("#tr-btn2-3").hide();
-                }
-            });
-        });
-        
-    </script> */}
       <div className="content-container">
         <div className="flex-child prediction">
           <button className="prediction-button" role="GenerateButton">
@@ -59,44 +61,6 @@ const MainContent = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>01-01-2020</td>
-                  <td>Pozitiv</td>
-                  <td>
-                    <button
-                      id="btn1"
-                      type="button"
-                      className="expand-button buttonInactive"
-                    >
-                      Expand details
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Smth 1</td>
-                  <td>Smth 2</td>
-                  <td>Smth 3</td>
-                </tr>
-                <tr>
-                  <td>Smth 1</td>
-                  <td>Smth 2</td>
-                  <td>Smth 3</td>
-                </tr>
-                <tr>
-                  <td>Smth 1</td>
-                  <td>Smth 2</td>
-                  <td>Smth 3</td>
-                </tr>
-                <tr>
-                  <td>Smth 1</td>
-                  <td>Smth 2</td>
-                  <td>Smth 3</td>
-                </tr>
-                <tr>
-                  <td>Smth 1</td>
-                  <td>Smth 2</td>
-                  <td>Smth 3</td>
-                </tr>
                 <tr>
                   <td>Smth 1</td>
                   <td>Smth 2</td>
@@ -197,118 +161,22 @@ const MainContent = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Maria</td>
-                  <td>Anders</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Francisco</td>
-                  <td>Chang</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>Roland</td>
-                  <td>Mendel</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Helen</td>
-                  <td>Bennett</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>5</td>
-                  <td>Yoshi</td>
-                  <td>Tannamuri</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>6</td>
-                  <td>Giovanni</td>
-                  <td>Rovelli</td>
-                  <td>
-                    <button className="pacient-button">Show prediction</button>
-                  </td>
-                </tr>
+                {
+                  patientsList.map((patient) => {
+                    setPatientsCounter((prev) => { return prev + 1}); 
+                    
+                    return (
+                      <tr key={patient.id}>
+                        <td>{patientsCounter}</td>
+                        <td>{patient.email}</td>
+                        <td>
+                          <button className="pacient-button">Show prediction</button>
+                        </td>
+                      </tr>
+                    ); 
+                  })
+                }
+               
               </tbody>
             </table>
           </div>
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -6,51 +6,49 @@ import { useState } from "react";
 import "./Auth.css";
 
 const Register = () => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  const userType = useSelector((state) => state.auth.userType);
-  const [error, setError] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const userType = useSelector((state) => state.auth.userType);
+    const [error, setError] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
 
-  const registerHandler = async (details) => {
-    //Check with Backend whether the given email already exists in the Database
-    //If already exists -> show error that email already exists
-    //Otherwise check the JWT for the type and send the request to the backend (must be logged in for this, otherwise -> login page)
-    //Test
-    const data = { email: details.userEmail, password: details.userPassword, firstName: details.userFirstName, lastName: details.userLastName, userType: userType === "admin" ? "medic" : "patient" }
-    try {
-      const response = await fetch('https://localhost:5001/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      if (response.status === 200) {
-        const res = await response.json()
-        setError("")
-        setSuccessMessage("New user added!")
-      }
-      else if (response.status === 409) {
-        setSuccessMessage("")
-        setError("This email is already in use!")
-      }
-      else {
-        throw new Error("Something went wrong!")
-      }
-    }
-    catch (error) {
-      console.log(error)
-    }
-  };
+    const registerHandler = async(details) => {
+        //Check with Backend whether the given email already exists in the Database
+        //If already exists -> show error that email already exists
+        //Otherwise check the JWT for the type and send the request to the backend (must be logged in for this, otherwise -> login page)
+        //Test
+        const data = { email: details.userEmail, password: details.userPassword, firstName: details.userFirstName, lastName: details.userLastName, userType: userType === "admin" ? "medic" : "patient" }
+        try {
+            const response = await fetch('https://localhost:5001/api/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            if (response.status === 200) {
+                const res = await response.json()
+                setError("")
+                setSuccessMessage("New user added!")
+            } else if (response.status === 409) {
+                setSuccessMessage("")
+                setError("This email is already in use!")
+            } else {
+                throw new Error("Something went wrong!")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
-  return (
-    <React.Fragment>
-      <div className="wrapper">
-        <RegisterForm registerHandler={registerHandler} error={error} success={successMessage} />
-      </div>
-    </React.Fragment>
-  );
+    return ( <React.Fragment >
+        <div className = "wrapper" >
+            <RegisterForm registerHandler = { registerHandler }
+            error = { error }
+            success = { successMessage }
+            /> </div> 
+        </React.Fragment>
+    );
 };
 
 export default Register;
